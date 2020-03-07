03d96f1eeaffd1b55e5deb5f5317e502bd969064
Shubha Patil <53892562+shubhapatil154@users.noreply.github.com>
2020-03-07 12:15:27 +0530

    Create SecondActivity.java

---
+++ Lab2_intents/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView t1,t2,t3;
+    String name,reg,dept;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        t1=  findViewById(R.id.textView1);
+        t2=  findViewById(R.id.textView2);
+        t3=  findViewById(R.id.textView3);
+
+        Intent i = getIntent();
+
+        name=i.getStringExtra("name_key");
+        reg=i.getStringExtra("reg_key");
+        dept=i.getStringExtra("dept_key");
+
+        t1.setText(name);
+        t2.setText(reg);
+        t3.setText(dept);
+
+    }
+
+}
