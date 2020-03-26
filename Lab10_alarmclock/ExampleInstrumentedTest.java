5ad319600bde26dffddab14d592715ecd69a1abb
Shubha Patil <53892562+shubhapatil154@users.noreply.github.com>
2020-03-26 16:55:53 +0530

    Update and rename Lab10_Alarm/ExampleInstrumentedTest.java to Lab10_alarmclock/ExampleInstrumentedTest.java

--- Lab10_alarmclock/ExampleInstrumentedTest.java
+++ Lab10_alarmclock/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.Lab10_Alarm;
+package com.example.alarmclock;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.lab10", appContext.getPackageName());
+        assertEquals("com.example.alarmclock", appContext.getPackageName());
     }
 }
