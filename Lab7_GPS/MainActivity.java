8ae5e3e3056ebf9e08b4c64022b54b91aca7eeb9
Shubha Patil <53892562+shubhapatil154@users.noreply.github.com>
2020-04-09 11:09:36 +0530

    Create MainActivity.java

---
+++ Lab7_GPS/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.gps;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    Button btnShowLocation;
+
+
+    GPStrace gps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnShowLocation = (Button)findViewById(R.id.button);
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gps = new GPStrace(MainActivity.this);
+                if (gps.getLocation() != null) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    Toast.makeText(getApplicationContext(), "Your Location is \nLat:" + latitude + "\nLong:" + longitude, Toast.LENGTH_LONG).show();
+                } else {
+//gps.showSettingAlert();
+                }
+            }
+        });
+    }
+}
